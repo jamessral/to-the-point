579e616e618c5ffaa7520515754bc871fa74e8d7
James A. Sral <jamessral@gmail.com>
2023-08-11 16:41:15 -0400

    Show computed properties

--- src/lib/Points.svelte
+++ src/lib/Points.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
 	export let options: string[] = [];
 
-	let selected = null;
+	let selected: string | null = null;
 
 	function handleSelect(option: string) {
 		if (options.includes(option)) {
 			selected = option;
 		}
 	}
+
+	$: selectedText = `${selected} is the current choice`
 </script>
 
 {#each options as option}
 	<button
 		on:click|preventDefault={() => handleSelect(option)}
-		class:outline={selected != option}
+		class:outline={selected !== option}
 		class:contrast={selected === option}
 	>
 		{option}
 	</button>
 {/each}
+
+{#if selected}
+	<p>{selectedText}</p>
+{/if}
